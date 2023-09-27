f7b693fdf8a834b5b02765104fc464b82f1103e9
Kyle J. Harms <kyle.harms@cornell.edu>
2023-09-27 15:31:31 -0400

    class 12 materials

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+
+import { useState } from 'react';
+
+import AccordionPanel from './components/AccordionPanel';
+import DarkModeSwitcher from './components/DarkModeSwitcher';
+
+export default function App() {
+  const reactDocs = [
+    {
+      title: "Props",
+      body: "React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions."
+    },
+    {
+      title: "State",
+      body: "Components often need to change what’s on the screen as a result of an interaction. Typing into the form should update the input field, clicking “next” on an image carousel should change which image is displayed, clicking “buy” should put a product in the shopping cart. Components need to “remember” things: the current input value, the current image, the shopping cart. In React, this kind of component-specific memory is called state."
+    },
+    {
+      title: "Events",
+      body: "React lets you add event handlers to your JSX. Event handlers are your own functions that will be triggered in response to interactions like clicking, hovering, focusing form inputs, and so on."
+    },
+    {
+      title: "Lifting State Up",
+      body: "Sometimes, you want the state of two components to always change together. To do it, remove state from both of them, move it to their closest common parent, and then pass it down to them via props. This is known as lifting state up, and it’s one of the most common things you will do writing React code."
+    }
+  ];
+
+  const [panelActiveIndex, setPanelActiveIndex] = useState(1);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const accordionPanels = reactDocs.map(doc => (
+    <AccordionPanel
+      title={doc.title}
+      isExpanded={panelActiveIndex === 1}
+      onActivate={() => setPanelActiveIndex(1)}
+      darkMode={isDarkMode}
+    >
+      {doc.body}
+    </AccordionPanel>
+  ));
+
+  return (
+    <div className="App" style={{ backgroundColor: (isDarkMode ? 'black' : 'white') }}>
+      <DarkModeSwitcher onSwitchDarkMode={setIsDarkMode} />
+
+      {accordionPanels}
+    </div>
+  );
+}
